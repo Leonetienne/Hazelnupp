ccfee5f98e4d9e6620a803cd5090494bc7b07159
Leonetienne <leonetienne@hotmail.de>
2021-06-03 12:22:25 +0200

    Added doxygen

---
+++ Doxygen/doxyrun.sh
@@ -0,0 +1,8 @@
+# Copy cut readme.md from root
+tail ../readme.md -n +5 > index.md
+
+# Run doxygen
+sudo doxygen doxyfig
+
+# Delete index afterwards to prevent people from editing it
+rm -f index.md
