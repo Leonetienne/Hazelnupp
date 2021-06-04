2a9e2f245931b13e1eed72864be8f5580c6b0c21
Leonetienne <leonetienne@hotmail.de>
2021-06-04 02:37:46 +0200

    Optimized generate.sh

--- INCLUDE/generate.sh
+++ INCLUDE/generate.sh
@@ -1,8 +1,7 @@
 # Build the include files
-echo Buildin the include files...
+echo "Building the include files..."
 echo ../Hazelnupp/*.cpp | sed 's/\.\.\/Hazelnupp\/main\.cpp//g' | xargs deggl -o Hazelnupp -i
 
 # Verify that they compile cleanly
-echo Verifying that they compile
-g++ Hazelnupp.cpp -c -Wall -Wextra -Wpedantic
-rm -f Hazelnupp.o
+echo "Verifying that they compile"
+g++ Hazelnupp.cpp -c -S -o - -Wall -Wextra -Wpedantic > /dev/null
