946691a74eb044e4cc566b87ca3ccfcea475449c
Leonetienne <leon@etiennes.de>
2022-04-28 12:11:12 +0200

    Remove tmp directory

--- tmp/INCLUDE/generate.sh
+++
@@ -1,7 +0,0 @@
-# Build the include files
-echo "Building the include files..."
-./deggl -i ../Hazelnupp/*.cpp --license-file ../license.txt -o Hazelnupp
-
-# Verify that they compile cleanly
-echo "Verifying that they compile"
-g++ Hazelnupp.cpp -c -S -std=c++17 -o - -Wall -Wextra -Wpedantic > /dev/null
--- tmp/INCLUDE/readme.md
+++
@@ -1,2 +0,0 @@
-You just need `Hazelnupp.h` and `Hazelnupp.cpp`.  
-The other files are just for compiling the big source in [/Hazelnupp](https://github.com/Leonetienne/Hazelnupp/tree/master/Hazelnupp) to these two files.
